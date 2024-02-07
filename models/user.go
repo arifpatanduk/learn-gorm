ee3c76f5b0273eeb4995385b388963eb9c149655
arifpatanduk <arifpatanduk2@gmail.com>
2024-02-07 10:46:38 +0700

    initial commit

---
+++ models/user.go
@@ -0,0 +1,36 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID uint `gorm:"primaryKey"`
+	Email string `gorm:"not null;unique;type:varchar(191)"`
+	Products []Product
+	CreatedAt time.Time 
+	UpdatedAt time.Time 
+}
+
+type Product struct {
+	ID uint `gorm:"primaryKey"`
+	Name string `gorm:"not null;type:varchar(191)"`
+	Brand string `gorm:"not null;type:varchar(191)"`
+	UserID uint
+	CreatedAt time.Time 
+	UpdatedAt time.Time 
+}
+
+func (p *Product) BeforeCreate(tx *gorm.DB) (err error) {
+	fmt.Println("Product before Create()")
+
+	if len(p.Name) < 4 {
+		err = errors.New("product name is too short")
+	}
+
+	return
+}
